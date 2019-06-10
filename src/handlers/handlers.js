addacf10c69de9be7a9e107cec40a7ef9808ce69
JD <johndoyle@gosilbo.com>
2019-06-10 15:16:19 +0100

    Remove original intent

--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -1,34 +1,29 @@
 import ClubInfoHandler from './ClubInfoHandler';
-import FifaRefHandler from './FifaRefHandler';
 import TeamInfoHandler from './TeamInfoHandler';
 
 const handlers = {
-  FifaRefIntent () {
-    const fifaRefHandler = new FifaRefHandler (this);
-    return fifaRefHandler.respond ();
+  TeamInfoIntent() {
+    const teamInfoHandler = new TeamInfoHandler(this);
+    return teamInfoHandler.respond();
   },
-  TeamInfoIntent () {
-    const teamInfoHandler = new TeamInfoHandler (this);
-    return teamInfoHandler.respond ();
+  ClubInfoIntent() {
+    const clubInfoHandler = new ClubInfoHandler(this);
+    return clubInfoHandler.respond();
   },
-  ClubInfoIntent () {
-    const clubInfoHandler = new ClubInfoHandler (this);
-    return clubInfoHandler.respond ();
-  },
-  LaunchRequest () {
-    this.emit (':ask', this.t ('WELCOME_PROMPT'), this.t ('WELCOME_REPROMPT'));
+  LaunchRequest() {
+    this.emit(':ask', this.t('WELCOME_PROMPT'), this.t('WELCOME_REPROMPT'));
   },
   'AMAZON.CancelIntent': function () {
-    this.emit (':tell', this.t ('GOODBYE'));
+    this.emit(':tell', this.t('GOODBYE'));
   },
   'AMAZON.HelpIntent': function () {
-    this.emit (':ask', this.t ('HELP_PROMPT'), this.t ('WELCOME_REPROMPT'));
+    this.emit(':ask', this.t('HELP_PROMPT'), this.t('WELCOME_REPROMPT'));
   },
   'AMAZON.StopIntent': function () {
-    this.emit (':tell', this.t ('GOODBYE'));
+    this.emit(':tell', this.t('GOODBYE'));
   },
-  SessionEndedRequest () {
-    this.emit (':tell', this.t ('GOODBYE'));
+  SessionEndedRequest() {
+    this.emit(':tell', this.t('GOODBYE'));
   },
 };
 
