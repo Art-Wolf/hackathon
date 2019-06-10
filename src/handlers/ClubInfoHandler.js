9ea8a5a6de8c0438a4be3fab6f8a11af8b2b4b90
JD <johndoyle@gosilbo.com>
2019-06-10 15:06:16 +0100

    Lets look for the state

--- src/handlers/ClubInfoHandler.js
+++ src/handlers/ClubInfoHandler.js
@@ -1,82 +1,70 @@
 // Load the AWS SDK for Node.js
-var AWS = require ('aws-sdk');
+var AWS = require('aws-sdk');
 // Set the region
-AWS.config.update ({region: 'us-west-2'});
-
-// Load the Google Distance Matrix API
-//var distance = require ('google-distance');
+AWS.config.update({ region: 'us-west-2' });
 
 export default class ClubInfoHandler {
-  constructor (alexa) {
+  constructor(alexa) {
     this.alexa = alexa;
     this.userProvidedState = this.alexa.event.request.intent.slots.state.value;
-    this.docClient = new AWS.DynamoDB.DocumentClient ();
+    this.docClient = new AWS.DynamoDB.DocumentClient();
   }
 
-  respond () {
-    if (!this.userProvidedState) return this.promptForState ();
+  respond() {
+    if (!this.userProvidedState) return this.promptForState();
 
     // Lets get all the clubs and states
-    this.getClubInfo ();
+    this.getClubInfo();
 
-    const say = `I do bid you a good day ${this.userProvidedState}`;
-    this.alexa.emit (':tell', say);
   }
 
-  promptForState () {
+  promptForState() {
     const say = 'No problem, what state do you live in?';
-    this.alexa.emit (':elicitSlot', 'state', say);
+    this.alexa.emit(':elicitSlot', 'state', say);
   }
 
-  getClubInfo () {
+  getClubInfo() {
     var params = {
       TableName: process.env.CLUB_INFO_TABLE,
       ProjectionExpression: 'id, team, city, state',
-      ExpressionAttributeNames: {
-        '#pfn': 'firstName',
-        '#pln': 'lastName',
-        '#pn': 'number',
+      FilterExpression: 'state = :p_state',
+      ExpressionAttributeValues: {
+        ':p_state': this.userProvidedState,
       },
     };
 
-    this.docClient.scan (params, this.onScan);
+    this.docClient.scan(params, this.onScan);
   }
 
-  onScan (err, data) {
+  onScan(err, data) {
     if (err) {
-      console.error (
+      console.error(
         'Unable to scan the table. Error JSON:',
-        JSON.stringify (err, null, 2)
+        JSON.stringify(err, null, 2)
       );
     } else {
       // print all the info
-      console.log ('Scan succeeded.');
-      data.Items.forEach (function (club) {
-        console.log (
-          club.id + ': ',
-          club.team + ' - ',
-          club.city + ', ',
-          club.state
-        );
-
-        //distance.get (
-        //  {
-        //    origin: this.userProvidedState,
-        //    destination: club.city + ', ' + club.state,
-        //  },
-        //  function (err, data) {
-        //    if (err) return console.log (err);
-        //    console.log (data);
-        //  }
-        //);
+      console.log('Scan succeeded.');
+      var dataFound = false;
+      data.Items.forEach(function (club) {
+        dataFound = true;
+        if (club.state == this.userProvidedState) {
+          const say = `Congrats! You're team is the ${club.team}`;
+          this.alexa.emit(':tell', say);
+        }
       });
+
+      if (!dataFound) {
+        const say = `I'm afraid you don't live in a state with a team. Do you here the list of teams?`;
+        this.alexa.emit(':ask', say);
+      }
 
       // continue scanning if we have more players, because
       // scan can retrieve a maximum of 1MB of data
       if (typeof data.LastEvaluatedKey != 'undefined') {
-        console.log ('Scanning for more...');
+        console.log('Scanning for more...');
         params.ExclusiveStartKey = data.LastEvaluatedKey;
-        this.docClient.scan (params, this.onScan);
+        this.docClient.scan(params, this.onScan);
       }
     }
   }
