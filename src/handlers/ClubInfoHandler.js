e9dfb4379ac6ec61c9ce21f6355153350b988497
JD <johndoyle@gosilbo.com>
2019-06-10 15:41:13 +0100

    bind the response

--- src/handlers/ClubInfoHandler.js
+++ src/handlers/ClubInfoHandler.js
@@ -36,41 +36,31 @@ export default class ClubInfoHandler {
       },
     };
 
-    return this.docClient.scan(params, this.onScan);
-  }
+    return this.docClient.scan(params, (err, data) => {
+      if (err) {
+        console.error(
+          'Unable to scan the table. Error JSON:',
+          JSON.stringify(err, null, 2)
+        );
+      } else {
+        // print all the info
+        console.log('Scan succeeded.');
+        var dataFound = false;
+        data.Items.forEach(function (club) {
+          dataFound = true;
+          if (club.state == this.userProvidedState) {
+            console.log('Team found: ' + club.team);
+            const say = `Congrats! You're team is the ${club.team}`;
+            this.alexa.emit(':tell', say);
+          }
+        });
 
-  onScan(err, data) {
-    if (err) {
-      console.error(
-        'Unable to scan the table. Error JSON:',
-        JSON.stringify(err, null, 2)
-      );
-    } else {
-      // print all the info
-      console.log('Scan succeeded.');
-      var dataFound = false;
-      data.Items.forEach(function (club) {
-        dataFound = true;
-        if (club.state == this.userProvidedState) {
-          console.log('Team found: ' + club.team);
-          const say = `Congrats! You're team is the ${club.team}`;
-          this.alexa.emit(':tell', say);
+        if (!dataFound) {
+          console.log('Team not found');
+          const say = `I'm afraid you don't live in a state with a team. Do you here the list of teams?`;
+          this.alexa.emit(':ask', say);
         }
-      });
-
-      if (!dataFound) {
-        console.log('Team not found');
-        const say = `I'm afraid you don't live in a state with a team. Do you here the list of teams?`;
-        this.alexa.emit(':ask', say);
       }
-
-      // continue scanning if we have more players, because
-      // scan can retrieve a maximum of 1MB of data
-      if (typeof data.LastEvaluatedKey != 'undefined') {
-        console.log('Scanning for more...');
-        params.ExclusiveStartKey = data.LastEvaluatedKey;
-        this.docClient.scan(params, this.onScan);
-      }
-    }
+    });
   }
 }
