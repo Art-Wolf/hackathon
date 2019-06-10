ca95fb7145d97aefee879e3e93c24badf89a1a94
JD <johndoyle@gosilbo.com>
2019-06-10 11:42:39 +0100

    Disable app id verification

--- src/alexa.js
+++ src/alexa.js
@@ -1,14 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import Alexa from 'alexa-sdk';
-import handlers from './handlers/handlers';
-import resources from './etc/resources';
 import Raven from 'raven';
 import RavenLambdaWrapper from 'serverless-sentry-lib';
+import resources from './etc/resources';
+import handlers from './handlers/handlers';
 
-export const handler = RavenLambdaWrapper.handler(Raven, (event, context, callback) => {
-  const alexa = Alexa.handler(event, context, callback);
-  alexa.appId = process.env.alexa_app_id;
-  alexa.resources = resources;
-  alexa.registerHandlers(...handlers);
-  alexa.execute();
-});
+export const handler = RavenLambdaWrapper.handler (
+  Raven,
+  (event, context, callback) => {
+    const alexa = Alexa.handler (event, context, callback);
+    //alexa.appId = process.env.alexa_app_id;
+    alexa.resources = resources;
+    alexa.registerHandlers (...handlers);
+    alexa.execute ();
+  }
+);
